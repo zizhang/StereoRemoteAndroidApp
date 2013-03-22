ee0142e9bed913015201a4b9c3b4507bcb852aa0
Nabeel Huq <nabeelhuq@host136-192.wifi.ubc.ca>
2013-03-22 10:34:14 -0700

    test

--- src/com/example/ece381/MainActivity.java
+++ src/com/example/ece381/MainActivity.java
@@ -49,7 +49,12 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
+		boolean bool = true;
+		return bool;
+		
+		
+		
+		////////////
 	}
 
 	// Route called when the user presses "connect"
